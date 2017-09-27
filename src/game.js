a6af6638776388965d09b76156a1f652c6258760
Jan Mrazek <mrazeja@fel.cvut.cz>
2017-09-27 17:46:46 +0200

    up to date version

--- src/game.js
+++ src/game.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-// game.js
-
-import Snake from './snake';
-
-class Game
-{
-  constructor()
-  {
-    this.snake = new Snake();
-    this.food = [];
-    this.canvas = document.createElement('canvas');
-    this.canvas.width = 100;
-    this.canvas.height = 100;
-    document.body.appendChild(canvas);
-    this.context = canvas.getContext('2d');
-  }
-  render()
-  {
-    this.snake.update();
-  }
-  update()
-  {
-    this.snake.render(this.context);
-  }
-}
-=======
 // game.js
 
 import Snake from './snake';
@@ -50,4 +23,3 @@ export default class Game {
     this.snake.render(this.context);
   }
 }
->>>>>>> a9ff6ebb44d73637fc9b5d25f574bcce7d7227ab
--- src/snake.js
+++ src/snake.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-// snake.js
-
-export default class Snake 
-{
-  constructor(x, y, segments)
-  {
-    this.body = [];
-    for(var i = 0; i < segments; i++)
-    {
-      this.body.push({x:x-i, y:y});
-    }
-    this.direction = 'right';
-  }
-
-  checkForConsumption(food)
-  {}
-
-  update()
-  {
-
-  }
-  render(ctx)
-  {
-    this.body.forEach(function(segment)
-    {
-      ctx.save();
-      ctx.fillStyle = 'green';
-      ctx.fillRect(segment.x, segment.y, 1, 1);
-    });
-    ctx.restore();
-
-  }
-}
-=======
 // Snake.js
 
 /** @class Snake
@@ -67,4 +32,3 @@ export default class Snake {
     })
   }
 }
->>>>>>> a9ff6ebb44d73637fc9b5d25f574bcce7d7227ab
